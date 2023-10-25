ecd481c48948540d8428bff593b31aaf9d06b9bb
Eugenio Giménez <63126781+eugeniogimenez@users.noreply.github.com>
2023-10-25 11:12:54 -0300

    Update rtdb.ts

--- client/rtdb.ts
+++ client/rtdb.ts
@@ -1,14 +1,4 @@
-// import firebase from "firebase";
 
-// const app = firebase.initializeApp({
-//   apiKey: "nDlevrKYq5Ca87qrMX1gOkLrAaZh81C75XiBN76T",
-//   authDomain: "dwf-m6-desafio-24f45.firebaseapp.com",
-//   databaseURL: "https://dwf-m6-desafio-24f45-default-rtdb.firebaseio.com",
-// });
-
-// const realtimeDB = firebase.database();
-
-// export { realtimeDB };
 
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
