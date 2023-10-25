8defe961d51b5986e5b6df040358683b70b56e2f
Eugenio Giménez <63126781+eugeniogimenez@users.noreply.github.com>
2023-10-25 11:11:14 -0300

    Update database.ts

--- server/database.ts
+++ server/database.ts
@@ -1,15 +1,4 @@
-// import * as admin from "firebase-admin";
-// const serviceKey = require("./clave.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceKey as any),
-//   databaseURL: "https://dwf-m6-desafio-24f45-default-rtdb.firebaseio.com",
-// });
-
-// const firestore = admin.firestore();
-// const realtimeDB = admin.database();
-
-// export { realtimeDB, firestore };
 
 import * as admin from "firebase-admin";
 import * as firebaseKey from "../key.json";
